200506ab66318222b6877bfbac6e591a9463c2f9
Alexis Davidson <alexisdavidson00@gmail.com>
2023-01-09 11:49:36 +0700

    functioning mobile

--- src/frontend/components/Mint.js
+++ src/frontend/components/Mint.js
@@ -36,11 +36,12 @@ const Mint = ({  }) => {
                     infoPrice="FREE MINT (2 PER WALLET)" infoDate="15 JANUARY 2023" infoTime="12PM UTC"
                     infoTotal="10,000 CARDS" infoNetwork="ETHEREUM" infoWhitelist="NO / FCFS" infoUtility="PROOF OF SCRATCH"
                     infoPrize="FREE MINT SPOT FOR SEASONAL SCRATCHY CARD"
-                    infoRoadmap="CLICK 'SEASONAL SCRATCHY CARD' BUTTON ABOVE" />
+                    infoRoadmap="CLICK 'SEASONAL SCRATCHY CARD' BUTTON ABOVE" 
+                    mobileTitle="GENESIS" />
             ) : (
                 <>
                     {/* PAGINATION ARROWS */}
-                    <div className="arrowsDiv">
+                    <div className="arrowsDiv displayDesktop">
                         {subpage > 1 ? (
                             <div className={"leftArrow"} onClick={() => nextPage(-1)}></div>
                         ) : ( <></> )}
@@ -52,28 +53,32 @@ const Mint = ({  }) => {
                     {/* CONTENT */}
                     {
                         {
-                            '1': <MintCard cardClass="scratchyCard1" mintActivated={false} 
+                            '1': <MintCard cardClass="scratchyCard1" mintActivated={false} nextPage={nextPage} subpage={subpage}
                                 infoPrice="FREE MINT" infoType="SEASON 1 SCRATCHY CARD" infoDate="25 JANUARY 2023" infoTime="12PM UTC"
                                 infoTotal="5,000 CARDS" infoNetwork="ETHEREUM" infoWhitelist="SCRATCH 2 GENESIS SCRATCHY CARD TO MINT 1 SEASON 1 SCRATCHY CARD"
                                 infoUtility="SCRATCH & WIN $COIN"
-                                infoPrize="UP TO 200 $COIN" />,
-                            '2': <MintCard cardClass="scratchyCard2" mintActivated={false} 
+                                infoPrize="UP TO 200 $COIN" 
+                                mobileTitle="SEASON 1" />,
+                            '2': <MintCard cardClass="scratchyCard2" mintActivated={false} nextPage={nextPage} subpage={subpage}
                                 infoPrice="FREE MINT" infoType="SEASON 2 SCRATCHY CARD" infoDate="5 FEBRUARY 2023" infoTime="12PM UTC"
                                 infoTotal="2,500 CARDS" infoNetwork="ETHEREUM" infoWhitelist="SCRATCH 2 GENESIS SCRATCHY CARD & 2 SEASON 1 SCRATCHY CARD TO MINT 1 SEASON 2 SCRATCHY CARD"
                                 infoUtility="SCRATCH & WIN PRIZE"
-                                infoPrize="MYSTERIOUS" />,
-                            '3': <MintCard cardClass="scratchyCardEmpty" cardText="?" mintActivated={false} 
+                                infoPrize="MYSTERIOUS" 
+                                mobileTitle="SEASON 2" />,
+                            '3': <MintCard cardClass="scratchyCardEmpty" cardText="?" mintActivated={false} nextPage={nextPage} subpage={subpage}
                                 infoPrice="FREE MINT" infoType="SEASON 3 SCRATCHY CARD" infoDate="15 FEBRUARY 2023" infoTime="12PM UTC"
                                 infoTotal="1,250 CARDS" infoNetwork="ETHEREUM"
                                 infoWhitelist="???"
                                 infoUtility="???"
-                                infoPrize="???" />,
-                            '4': <MintCard cardClass="scratchyCardEmpty" cardText="?" mintActivated={false} 
+                                infoPrize="???" 
+                                mobileTitle="SEASON 3" />,
+                            '4': <MintCard cardClass="scratchyCardEmpty" cardText="?" mintActivated={false} nextPage={nextPage} subpage={subpage}
                                 infoPrice="FREE MINT" infoType="SEASON 4 ONWARDS" infoDate="???" infoTime="???"
                                 infoTotal="???" infoNetwork="ETHEREUM"
                                 infoWhitelist="AMOUNT OF MINT BASED ON THE ??? GENESIS SCRATCHY CARD???"
                                 infoUtility="???"
-                                infoPrize="???" />,
+                                infoPrize="???" 
+                                mobileTitle="SEASON 4" />,
                         } [subpage]
                     }
                 </>
--- src/frontend/components/MintCard.js
+++ src/frontend/components/MintCard.js
@@ -5,34 +5,82 @@ import { Image, Row, Col, Button } from 'react-bootstrap'
 const fromWei = (num) => ethers.utils.formatEther(num)
 const toWei = (num) => ethers.utils.parseEther(num.toString())
 
-const MintCard = ({ mintActivated, nft, cardClass, cardText, 
-    infoPrice, infoType, infoDate, infoTime, infoTotal, infoNetwork, infoWhitelist, infoUtility, infoPrize, infoRoadmap }) => {
+const MintCard = ({ mintActivated, nft, cardClass, cardText, nextPage, subpage,
+    infoPrice, infoType, infoDate, infoTime, infoTotal, infoNetwork, infoWhitelist, infoUtility, infoPrize, infoRoadmap,
+    mobileTitle }) => {
+
+    const [displayCardInfo, setDisplayCardInfo] = useState(false)
 
     return (
         <Row className="m-0 p-0">
-            <Col className="col-8 m-0 p-0">
-                <div className={"scratchyCard " + cardClass} >
-                    {cardText}
-                </div>
-            </Col>
-            <Col className="col-4 m-0 p-0">
-                {mintActivated ? (
-                    <>Mint Activated</>
-                ) : (
-                    <div>
-                        PRICE: {infoPrice}
-                        {infoType ? ( <><br/>TYPE: {infoType}</> ) : ( <></> )}
-                        <br/>DATE: {infoDate}
-                        <br/>TIME: {infoTime}
-                        <br/>TOTAL: {infoTotal}
-                        <br/>NETWORK: {infoNetwork}
-                        <br/>WHITELIST: {infoWhitelist}
-                        <br/>UTILITY: {infoUtility}
-                        <br/>PRIZE: {infoPrize}
-                        {infoRoadmap ? ( <><br/>ROADMAP: {infoRoadmap}</> ) : ( <></> )}
+            <Row className="m-0 p-0">
+                <Col className="col-12 col-xl-8 m-0 p-0">
+                    <div className={"scratchyCard " + cardClass} >
+                        {cardText}
                     </div>
+                </Col>
+                <Col className="col-12 col-xl-4 m-0 p-0 displayDesktop">
+                    {mintActivated ? (
+                        <>Mint Activated</>
+                    ) : (
+                        <div>
+                            PRICE: {infoPrice}
+                            {infoType ? ( <><br/>TYPE: {infoType}</> ) : ( <></> )}
+                            <br/>DATE: {infoDate}
+                            <br/>TIME: {infoTime}
+                            <br/>TOTAL: {infoTotal}
+                            <br/>NETWORK: {infoNetwork}
+                            <br/>WHITELIST: {infoWhitelist}
+                            <br/>UTILITY: {infoUtility}
+                            <br/>PRIZE: {infoPrize}
+                            {infoRoadmap ? ( <><br/>ROADMAP: {infoRoadmap}</> ) : ( <></> )}
+                        </div>
+                    )}
+                </Col>
+            </Row>
+            <Row className="m-0 p-0 displayMobile">
+                <div>
+                    TAP THE CARD FOR MORE INFO
+                </div>
+                <div className="mobileMintBottomMenu">
+                    <div className="arrowDiv">
+                        {subpage > 1 ? (
+                            <div className={"leftArrow"} onClick={() => nextPage(-1)}></div>
+                        ) : ( <></> )}
+                    </div>
+                    <div className="subNavbarMenu" onClick={() => setDisplayCardInfo(true)} >
+                        {mobileTitle}
+                    </div>
+                    <div className="arrowDiv">
+                        {subpage < 4 ? (
+                            <div className={"rightArrow"} onClick={() => nextPage(1)}></div>
+                        ) : ( <></> )}
+                    </div>
+                </div>
+
+                {displayCardInfo ? (
+                    <Row className="popupBigSmallText">
+                        <div>
+                            PRICE: {infoPrice}
+                            {infoType ? ( <><br/>TYPE: {infoType}</> ) : ( <></> )}
+                            <br/>DATE: {infoDate}
+                            <br/>TIME: {infoTime}
+                            <br/>TOTAL: {infoTotal}
+                            <br/>NETWORK: {infoNetwork}
+                            <br/>WHITELIST: {infoWhitelist}
+                            <br/>UTILITY: {infoUtility}
+                            <br/>PRIZE: {infoPrize}
+                            {infoRoadmap ? ( <><br/>ROADMAP: {infoRoadmap}</> ) : ( <></> )}
+                        </div>
+
+                        <div className="popupButtonClose mt-3" onClick={() => setDisplayCardInfo(false)} >
+                            Close
+                        </div>
+                    </Row>
+                ) : (
+                    <></>
                 )}
-            </Col>
+            </Row>
         </Row>
     );
 }
