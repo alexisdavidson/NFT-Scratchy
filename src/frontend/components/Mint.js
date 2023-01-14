3a720611b4e7f9a11926a1570013400badda78be
Alexis Davidson <alexisdavidson00@gmail.com>
2023-01-14 10:58:17 +0700

    update dates

--- src/frontend/components/Mint.js
+++ src/frontend/components/Mint.js
@@ -34,7 +34,7 @@ const Mint = ({ account, nft, web3Handler, balance }) => {
             {submenu == 1 ? (
                 <MintCard account={account} web3Handler={web3Handler} nft={nft} balance={balance}
                     cardClass="scratchyCardGenesis" mintActivated={true}
-                    infoPrice="FREE MINT (2 PER WALLET)" infoDate="15 JANUARY 2023" infoTime="12PM UTC"
+                    infoPrice="FREE MINT (2 PER WALLET)" infoDate="21 JANUARY 2023" infoTime="12PM UTC"
                     infoTotal="10,000 CARDS" infoNetwork="ETHEREUM" infoWhitelist="NO / FCFS" infoUtility="PROOF OF SCRATCH"
                     infoPrize="FREE MINT SPOT FOR SEASONAL SCRATCHY CARD"
                     infoRoadmap="CLICK 'SEASONAL SCRATCHY CARD' BUTTON ABOVE" 
@@ -56,21 +56,21 @@ const Mint = ({ account, nft, web3Handler, balance }) => {
                         {
                             '1': <MintCard account={account} web3Handler={web3Handler} 
                                 cardClass="scratchyCard1" mintActivated={false} nextPage={nextPage} subpage={subpage}
-                                infoPrice="FREE MINT" infoType="SEASON 1 SCRATCHY CARD" infoDate="25 JANUARY 2023" infoTime="12PM UTC"
+                                infoPrice="FREE MINT" infoType="SEASON 1 SCRATCHY CARD" infoDate="31 JANUARY 2023" infoTime="12PM UTC"
                                 infoTotal="5,000 CARDS" infoNetwork="ETHEREUM" infoWhitelist="SCRATCH 2 GENESIS SCRATCHY CARD TO MINT 1 SEASON 1 SCRATCHY CARD"
                                 infoUtility="SCRATCH & WIN $COIN"
                                 infoPrize="UP TO 200 $COIN" 
                                 mobileTitle="SEASON 1" />,
                             '2': <MintCard account={account} web3Handler={web3Handler} 
                                 cardClass="scratchyCard2" mintActivated={false} nextPage={nextPage} subpage={subpage}
-                                infoPrice="FREE MINT" infoType="SEASON 2 SCRATCHY CARD" infoDate="5 FEBRUARY 2023" infoTime="12PM UTC"
+                                infoPrice="FREE MINT" infoType="SEASON 2 SCRATCHY CARD" infoDate="11 FEBRUARY 2023" infoTime="12PM UTC"
                                 infoTotal="2,500 CARDS" infoNetwork="ETHEREUM" infoWhitelist="SCRATCH 2 GENESIS SCRATCHY CARD & 2 SEASON 1 SCRATCHY CARD TO MINT 1 SEASON 2 SCRATCHY CARD"
                                 infoUtility="SCRATCH & WIN PRIZE"
                                 infoPrize="MYSTERIOUS" 
                                 mobileTitle="SEASON 2" />,
                             '3': <MintCard account={account} web3Handler={web3Handler} 
                                 cardClass="scratchyCardEmpty" cardText="?" mintActivated={false} nextPage={nextPage} subpage={subpage}
-                                infoPrice="FREE MINT" infoType="SEASON 3 SCRATCHY CARD" infoDate="15 FEBRUARY 2023" infoTime="12PM UTC"
+                                infoPrice="FREE MINT" infoType="SEASON 3 SCRATCHY CARD" infoDate="21 FEBRUARY 2023" infoTime="12PM UTC"
                                 infoTotal="1,250 CARDS" infoNetwork="ETHEREUM"
                                 infoWhitelist="???"
                                 infoUtility="???"
